67c97595c257cedc20deead66c598597ad7d3feb
abaton <abaton@localhost.localdomain>
2019-08-09 13:53:57 -0400

    wrote a webserver

---
+++ index.js
@@ -0,0 +1,24 @@
+let express = require('express')
+let https = require('https')
+let fs = require('fs')
+
+
+let app = express()
+//this is for setting up the ssl server
+let options = {
+    key: fs.readFileSync(process.env['SERVER_KEY']),
+    cert: fs.readFileSync(process.enc['SERVER_CERT']),
+    passphrase: process.env['SERVER_PASS']
+}
+
+app.use('/', (req, res)=>{
+	res.json('Hello world')
+})
+
+let listener = https.createServer(options, app).listen(4000, ()=>{
+	console.log('App is Live')
+})
+
+
+
+
